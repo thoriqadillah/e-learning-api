c519add115ded4fe35f1483d13ec0d200c123e92
thoriqadillah <thoriqadillah59@gmail.com>
2022-03-10 16:33:48 +0700

    add db connection

---
+++ config/db-config.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+
+//For creating database connection
+func SetupDB() {
+	errEnv := godotenv.Load()
+	if errEnv != nil {
+		panic(errEnv.Error())
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:3308)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbName)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	DB = db
+}
+
+//For closing database connection
+func CloseDB() {
+	dbCon, err := DB.DB()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	dbCon.Close()
+}
