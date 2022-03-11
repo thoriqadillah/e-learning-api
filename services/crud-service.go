809825709f6ac05439f4c80f1ca1cda60fb3c0ad
thoriqadillah <thoriqadillah59@gmail.com>
2022-03-11 13:52:58 +0700

    add readOne for reatrieving single data

--- services/crud-service.go
+++ services/crud-service.go
@@ -5,6 +5,7 @@ import "github.com/gin-gonic/gin"
 type Crud interface {
 	Create(ctx *gin.Context)
 	Read(ctx *gin.Context)
+	ReadOne(ctx *gin.Context)
 	Update(ctx *gin.Context)
 	Delete(ctx *gin.Context)
 }
