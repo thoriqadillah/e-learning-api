24fdbd6ac2e253f8c30e77c5e1975d758b45f5c8
thoriqadillah <thoriqadillah59@gmail.com>
2022-03-11 13:55:29 +0700

    add email binding

--- dto/mahasiswa-dto.go
+++ dto/mahasiswa-dto.go
@@ -2,6 +2,6 @@ package dto
 
 type MahasiswaUpdate struct {
 	Name     string `json:"name"`
-	Email    string `json:"email"`
+	Email    string `json:"email" binding:"email"`
 	Password string `json:"password,omitempty" binding:"max=15,alphanum" `
 }
