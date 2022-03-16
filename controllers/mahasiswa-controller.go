e63bc4d9064628a5344af0908fc3a87b31b80e42
thoriqadillah <thoriqadillah59@gmail.com>
2022-03-16 14:08:34 +0700

    add crud functionality

--- controllers/mahasiswa-controller.go
+++ controllers/mahasiswa-controller.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"github.com/thoriqadillah/e-learning-api/config"
+	"github.com/thoriqadillah/e-learning-api/dto"
 	"github.com/thoriqadillah/e-learning-api/helpers"
 	"github.com/thoriqadillah/e-learning-api/models"
 	"github.com/thoriqadillah/e-learning-api/services"
@@ -39,12 +40,59 @@ func (m *mahasiswaController) Read(ctx *gin.Context) {
 
 	err := config.DB.Find(&mahasiswa).Error
 	if err != nil {
-		panic("Error retrieving mahasiswa : " + err.Error())
+		ctx.JSON(http.StatusNotFound, helpers.BuildResponse("Data not found", nil))
+		return
 	}
 
 	ctx.JSON(http.StatusOK, helpers.BuildResponse("Success", &mahasiswa))
 }
 
-func (m *mahasiswaController) ReadOne(ctx *gin.Context) {}
-func (m *mahasiswaController) Update(ctx *gin.Context)  {}
-func (m *mahasiswaController) Delete(ctx *gin.Context)  {}
+func (m *mahasiswaController) ReadOne(ctx *gin.Context) {
+	var mahasiswa models.Mahasiswa
+
+	err := config.DB.First(&mahasiswa, ctx.Param("id")).Error
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, helpers.BuildResponse("Data not found", nil))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, helpers.BuildResponse("Success", &mahasiswa))
+}
+
+func (m *mahasiswaController) Update(ctx *gin.Context) {
+	var mahasiswa dto.Mahasiswa
+	id := ctx.Param("id")
+
+	if err := ctx.ShouldBindJSON(&mahasiswa); err != nil {
+		res := []string{}
+		for _, er := range err.(validator.ValidationErrors) {
+			res = append(res, "Error on field "+er.Field())
+		}
+
+		ctx.JSON(http.StatusBadRequest, helpers.BuildErrorResponse(res, nil))
+		return
+	}
+
+	err := config.DB.Model(&mahasiswa).Where("id = ?", id).Updates(&mahasiswa).Error
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, helpers.BuildResponse("Data not found", nil))
+		return
+	}
+
+	config.DB.Save(&mahasiswa)
+	ctx.JSON(http.StatusOK, helpers.BuildResponse("Success", nil))
+}
+
+func (m *mahasiswaController) Delete(ctx *gin.Context) {
+	var mahasiswa dto.Mahasiswa
+	id := ctx.Param("id")
+
+	err := config.DB.Where("id = ?", id).First(&mahasiswa).Error
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, helpers.BuildResponse("Data not found", nil))
+		return
+	}
+
+	config.DB.Where("id = ?", id).Delete(&mahasiswa)
+	ctx.JSON(http.StatusOK, helpers.BuildResponse("Success", nil))
+}
